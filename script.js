66224c2874a6f515baf61f800d6feb9ecf8679f8
Gergi83 <georgiyordanov@protonmail.com>
2021-05-10 17:58:31 +0300

    css and dark mode functionality added

---
+++ script.js
@@ -0,0 +1,14 @@
+toggle.addEventListener('click', (e) => {
+  const html = document.querySelector('html');
+  if (html.classList.contains('dark')) {
+    html.classList.remove('dark');
+    e.target.innerHTML = 'Dark mode';
+  } else {
+    html.classList.add('dark');
+    e.target.innerHTML = 'Light mode';
+  }
+});
+
+function setTime() {
+  const time = new Date();
+}
